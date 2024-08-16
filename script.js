980b8b450d9c93d7f24e9b41edf3d2ea7254fbc6
Mr.Marshal <marshalrock11@gmail.com>
2024-08-16 15:16:36 +1000

    Added button, and changed CSS a bit

--- script.js
+++ script.js
@@ -4,5 +4,12 @@ const container = document.querySelector("#container");
 for (i = 1; i <= (16 * 16); i++) {
     const grid = document.createElement('div');
     grid.classList.add('grid');
+    grid.addEventListener('mouseover', () => {
+        grid.style.cssText = 'background-color: red;';
+    })
+    grid.addEventListener('mouseout', () => {
+        grid.style.cssText = 'background-color: white;';
+    })
     container.appendChild(grid);
 }
+
