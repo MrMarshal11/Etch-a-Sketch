2c962f72ff36f5f0f129bd442155bfe4d26cbb41
Mr.Marshal <marshalrock11@gmail.com>
2024-08-16 14:58:24 +1000

    Created 16*16 grid

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const container = document.querySelector("#container");
 // creating 16*16 grid
 for (i = 1; i <= (16 * 16); i++) {
     const grid = document.createElement('div');
-    container.appendChild(grid)
+    grid.classList.add('grid');
+    container.appendChild(grid);
 }
